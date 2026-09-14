96cfc01bab58f8df8506b7c0e7936fbc32bc2cb5
BrashannnStudios <brashannnstudios@gmail.com>
2026-09-14 16:28:59 -0300

    Update moderation.py

--- moderation.py
+++ moderation.py
@@ -264,7 +264,7 @@ class Moderation(commands.Cog):
     async def cmds(self, ctx: commands.Context):
         embed = base_embed(
             f"{EMOJI_SEARCH} Command List",
-            "Prefix: `?` ",
+            "Prefix: `?` (case-insensitive)",
             COLOR_INFO,
         )
         embed.add_field(
